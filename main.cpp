147baa25bf9617542049a060aa75863a4cac6674
Pavel Filippenko <filippenko.ps@phystech.edu>
2024-06-18 14:21:38 +0300

    [fix cmd options]

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <sys/inotify.h>
 #include <pthread.h>
 #include <getopt.h>
+#include <getopt.h>
 
 #include <unordered_map>
 #include <ctime>
@@ -22,7 +23,9 @@
 #include "directory.hpp"
 #include "crc32.hpp"
 #include "event_handler.hpp"
-#include "arg_parser.hpp"
+
+static char *path_to_directory = NULL;
+int periode = 60;
 
 // нельзя оптимизировать использование этой переменной
 volatile int cought_signum;
@@ -35,6 +38,46 @@ void sig_handler(int signum, siginfo_t *info, void *ctx) {
         printf("[DEBUG]\n");
     }
     proc_pid = info->si_pid;
+}
+
+static void show_help() {
+    printf ("--help, -h\n"
+            "\tPrint this message and exit\n"
+            "\n"
+            "--directory, -d\n"
+            "\tPath to the directory demon look after\n"
+            "\n"
+            "--periode, -p\n"
+            "\tPeriode in sec for the demon\n");
+}
+
+static void parse_args (int argc, char **argv) {
+    int res = 0, ind = 0;
+    const char *short_opts = "hf:";
+    const struct option long_opts[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"directory", required_argument, NULL, 'd'},
+        {"periode", required_argument, NULL, 'p'},
+        {NULL, 0, NULL, 0}
+    };
+
+    while ((res = getopt_long(argc, argv, short_opts, long_opts, &ind)) != -1)
+        switch (res)
+        {
+            default:
+            case 'h': {
+                show_help();
+                return;
+            }
+            case 'd': {
+                path_to_directory = optarg;
+                break;
+            }
+            case 'p': {
+                periode = atoi(optarg);
+                break;
+            }
+        }
 }
 
 int main(int argc, char* argv[]) {
